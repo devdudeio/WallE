69a203a2f039f0948141330221a7b4440700bfdf
Robert Lech <robert.lech@icloud.com>
2015-10-15 02:05:37 +0200

    init

---
+++ mobile-config.js
@@ -0,0 +1,10 @@
+// This section sets up some basic app metadata,
+// the entire section is optional.
+App.info({
+    id: 'app.walle',
+    name: 'WallE',
+    description: '',
+    author: 'Robert Lech',
+    email: 'robert.lech@icloud.com',
+    website: 'http://example.com'
+});
